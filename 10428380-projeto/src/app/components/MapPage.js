8aa1b4167c25b17cc368786a59a811a758bd1570
guigasoares <143802004+guigasoares@users.noreply.github.com>
2024-05-27 00:24:10 -0300

    Add files via upload

--- 10428380-projeto/src/app/components/MapPage.js
+++ 10428380-projeto/src/app/components/MapPage.js
@@ -1,12 +1,34 @@
-'use client'
-import React, { useState } from 'react';
-function MapPage() {
-    return (
-      <div>
-        <h1 className='findacenter'>Encontre os Centros de Apoio a partir do Mapa:</h1>
-        <iframe title="Mapa de Higienópolis, São Paulo" width="100%" height="700" loading='lazy' allowFullScreen src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3657.239695574176!2d-46.65624468472896!3d-23.5461664846808!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce58cde41618d1%3A0xed5038f90c2a4c0!2sUniversidade%20Presbiteriana%20Mackenzie%20-%20Higien%C3%B3polis!5e0!3m2!1sen!2sbr!4v1617982787875!5m2!1sen!2sbr" style={{ border: 0 }}></iframe>
-      </div>
-    );
-  }
+import React from 'react';
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import 'leaflet/dist/leaflet.css'; // Importe o CSS do Leaflet
 
-  export default MapPage;
+export default function MapPage({ centrosDeAjuda }) {
+  return (
+    <div>
+      <h1 className='findacenter'>Encontre Centros de Ajuda contra o Alcoolismo e Substâncias Tóxicas:</h1>
+      <div style={{ height: '500px', width: '100%' }}>
+        <MapContainer
+          center={[-23.5489, -46.6388]}
+          zoom={13}
+          scrollWheelZoom={true}
+          style={{ height: '100%', width: '100%' }}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          {centrosDeAjuda && centrosDeAjuda.map((centro) => (
+            <Marker key={centro.id} position={[centro.latitude, centro.longitude]}>
+              <Popup>
+                <h1>{centro.nome}</h1>
+                <p><strong>Endereço: </strong>{centro.endereco}</p>
+                <p><strong>Telefone: </strong>{centro.telefone}</p>
+                <p><strong>Horário de Funcionamento: </strong>{centro.funcionamento}</p>
+              </Popup>
+            </Marker>
+          ))}
+        </MapContainer>
+      </div>
+    </div>
+  );
+}
--- 10428380-projeto/src/app/components/SupportPage.js
+++ 10428380-projeto/src/app/components/SupportPage.js
@@ -1,11 +1,35 @@
 'use client'
 import React, { useState } from 'react';
 function SupportPage() {
+
+ const [showFullText8, setShowFullText8] = useState(false);
+    const [showFullText9, setShowFullText9] = useState(false);
+    const [showFullText10, setShowFullText10] = useState(false);
+    const [showFullText11, setShowFullText11] = useState(false);
+  
+    const handleToggleText8 = () => {
+      setShowFullText8(!showFullText8);
+    };
+  
+    const handleToggleText9 = () => {
+      setShowFullText9(!showFullText9);
+    };
+  
+    const handleToggleText10 = () => {
+      setShowFullText10(!showFullText10);
+    };
+  
+    const handleToggleText11 = () => {
+      setShowFullText11(!showFullText11);
+    };
+  
     return (
       <div className="home-container">
       <h1 className="inicial">Quais são os principais recursos de apoio?</h1>
       <div className="boxIeD">
-        <h2>Tratamento Médico e Desintoxicação</h2>
+      <h2>Tratamento Médico e Desintoxicação</h2>
+      {showFullText8 ? (
+    <>
         <p>Na luta contra o vício de substâncias ilegais, o Tratamento Médico e a Desintoxicação se erguem como pilares fundamentais para a recuperação e a construção de um futuro livre das drogas. Mais do que simples medidas isoladas, representam um processo holístico e multifacetado que visa atacar o vício em suas diversas frentes, desde os aspectos físicos até os psicológicos e sociais.</p>
         <p><strong>Compreendendo a Complexidade do Vício:</strong></p>
         <p>O vício é uma doença complexa e crônica que afeta o cérebro e o comportamento do indivíduo. Caracteriza-se por um desejo compulsivo de usar substâncias, mesmo diante de consequências negativas, e pela perda de controle sobre o uso. Essa compulsão é impulsionada por alterações neuroquímicas no cérebro, que tornam cada vez mais difícil resistir à droga.</p>
@@ -22,44 +46,101 @@ function SupportPage() {
         <p><strong>Um Caminho para a Recuperação:</strong></p>
         <p>O Tratamento Médico e a Desintoxicação não são soluções mágicas ou atalhos para a cura. Representam o início de um árduo processo de recuperação que exige dedicação, persistência e apoio profissional contínuo. No entanto, são pilares indispensáveis para construir uma vida livre do vício e alcançar o pleno potencial.</p>
         <p>Juntos, podemos construir uma comunidade livre do vício e com um futuro promissor para todos!</p>
-  
+    <button onClick={handleToggleText8}>Mostrar Menos</button>
+  </>
+  ) : (
+  <>
+  <p>Na luta contra o vício de substâncias ilegais, o Tratamento Médico e a Desintoxicação se erguem como pilares fundamentais para a recuperação e a construção de um futuro livre das drogas. Mais do que simples medidas isoladas, representam um processo holístico e multifacetado que visa atacar o vício em suas diversas frentes, desde os aspectos físicos até os psicológicos e sociais.</p>
+        <p><strong>Compreendendo a Complexidade do Vício:</strong></p>
+        <p>O vício é uma doença complexa e crônica que afeta o cérebro e o comportamento do indivíduo. Caracteriza-se por um desejo compulsivo de usar substâncias, mesmo diante de consequências negativas, e pela perda de controle sobre o uso. Essa compulsão é impulsionada por alterações neuroquímicas no cérebro, que tornam cada vez mais difícil resistir à droga.</p>
+        <p><strong>O Papel Essencial do Tratamento Médico:</strong></p>
+    <button onClick={handleToggleText8}>Mostrar Mais</button>
+  </>
+  )}
+
         </div>
       <div className="boxIeD">
         <h2>Terapia e Aconselhamento</h2>
-        <p><strong>Guiando a Jornada de Recuperação:</strong></p>
-        <p>Na luta contra o vício de substâncias ilegais, Terapia e Aconselhamento se revelam ferramentas valiosas para auxiliar o indivíduo em sua jornada de recuperação. Mais do que simples conversas, representam um processo profundo e personalizado que visa abordar os aspectos psicológicos, sociais e emocionais do vício, promovendo a mudança de comportamento e a construção de uma vida livre das drogas.</p>
-        <p><strong>Mergulhando nas Profundezas do Vício:</strong></p>
-        <p>O vício é uma doença complexa que transcende os limites físicos da dependência química. Envolve questões emocionais, comportamentais e sociais que, se não forem devidamente exploradas e tratadas, podem comprometer o processo de recuperação. É nesse contexto que a Terapia e o Aconselhamento assumem um papel crucial.</p>
-        <p><strong>A Jornada da Autodescoberta:</strong></p>
-        <p>Através de um ambiente seguro e confidencial, a Terapia oferece ao indivíduo a oportunidade de explorar seus sentimentos, pensamentos e comportamentos relacionados ao vício. Com o auxílio de um profissional qualificado, ele pode identificar os gatilhos, compreender as raízes do vício, desenvolver habilidades de enfrentamento, fortalecer a autoestima.</p>
-        <p><strong>O Poder do Aconselhamento:</strong></p>
-        <p>O Aconselhamento, por sua vez, oferece um espaço para o indivíduo discutir seus objetivos, desafios e progressos na jornada de recuperação. Com base em sua experiência e conhecimento, o profissional de aconselhamento pode orientar e motivar, educar sobre o vício, desenvolver um plano de recuperação individualizado, conectar com recursos de apoio. </p>
-        <p><strong>Terapia e Aconselhamento: Uma Jornada Conjunta:</strong></p>
-        <p>Terapia e Aconselhamento não são soluções isoladas, mas sim ferramentas complementares que, quando utilizadas em conjunto, potencializam os resultados da recuperação. Ao abordar os aspectos psicológicos, sociais e emocionais do vício, essas ferramentas auxiliam o indivíduo a desenvolver resiliência, fortalecer sua autoestima e construir uma vida livre das drogas.</p>
-        <p>Existem diversos recursos disponíveis para auxiliar na recuperação, como grupos de apoio, clínicas especializadas e linhas de apoio telefônico.</p>
-  
+        {showFullText9 ? (
+          <>
+          <p><strong>Guiando a Jornada de Recuperação:</strong></p>
+          <p>Na luta contra o vício de substâncias ilegais, Terapia e Aconselhamento se revelam ferramentas valiosas para auxiliar o indivíduo em sua jornada de recuperação. Mais do que simples conversas, representam um processo profundo e personalizado que visa abordar os aspectos psicológicos, sociais e emocionais do vício, promovendo a mudança de comportamento e a construção de uma vida livre das drogas.</p>
+          <p><strong>Mergulhando nas Profundezas do Vício:</strong></p>
+          <p>O vício é uma doença complexa que transcende os limites físicos da dependência química. Envolve questões emocionais, comportamentais e sociais que, se não forem devidamente exploradas e tratadas, podem comprometer o processo de recuperação. É nesse contexto que a Terapia e o Aconselhamento assumem um papel crucial.</p>
+          <p><strong>A Jornada da Autodescoberta:</strong></p>
+          <p>Através de um ambiente seguro e confidencial, a Terapia oferece ao indivíduo a oportunidade de explorar seus sentimentos, pensamentos e comportamentos relacionados ao vício. Com o auxílio de um profissional qualificado, ele pode identificar os gatilhos, compreender as raízes do vício, desenvolver habilidades de enfrentamento, fortalecer a autoestima.</p>
+          <p><strong>O Poder do Aconselhamento:</strong></p>
+          <p>O Aconselhamento, por sua vez, oferece um espaço para o indivíduo discutir seus objetivos, desafios e progressos na jornada de recuperação. Com base em sua experiência e conhecimento, o profissional de aconselhamento pode orientar e motivar, educar sobre o vício, desenvolver um plano de recuperação individualizado, conectar com recursos de apoio. </p>
+          <p><strong>Terapia e Aconselhamento: Uma Jornada Conjunta:</strong></p>
+          <p>Terapia e Aconselhamento não são soluções isoladas, mas sim ferramentas complementares que, quando utilizadas em conjunto, potencializam os resultados da recuperação. Ao abordar os aspectos psicológicos, sociais e emocionais do vício, essas ferramentas auxiliam o indivíduo a desenvolver resiliência, fortalecer sua autoestima e construir uma vida livre das drogas.</p>
+          <p>Existem diversos recursos disponíveis para auxiliar na recuperação, como grupos de apoio, clínicas especializadas e linhas de apoio telefônico.</p>
+          <button onClick={handleToggleText9}>Mostrar Menos</button>
+          </>
+        ) : (
+          <>
+          <p><strong>Guiando a Jornada de Recuperação:</strong></p>
+          <p>Na luta contra o vício de substâncias ilegais, Terapia e Aconselhamento se revelam ferramentas valiosas para auxiliar o indivíduo em sua jornada de recuperação. Mais do que simples conversas, representam um processo profundo e personalizado que visa abordar os aspectos psicológicos, sociais e emocionais do vício, promovendo a mudança de comportamento e a construção de uma vida livre das drogas.</p>
+          <p><strong>Mergulhando nas Profundezas do Vício:</strong></p>
+          <p>O vício é uma doença complexa que transcende os limites físicos da dependência química. Envolve questões emocionais, comportamentais e sociais que, se não forem devidamente exploradas e tratadas, podem comprometer o processo de recuperação. É nesse contexto que a Terapia e o Aconselhamento assumem um papel crucial.</p>
+          <p><strong>A Jornada da Autodescoberta:</strong></p>
+          <button onClick={handleToggleText9}>Mostrar Mais</button>
+          </>
+        )
+      }
       </div>
       <div className="boxIeD">
         <h2>Grupos de Apoio</h2>
+
+        {showFullText10 ? (
+          <>
         <p><strong>A Força da Comunidade na Jornada de Recuperação:</strong></p>
         <p>Na luta contra o vício de substâncias ilegais, os Grupos de Apoio se erguem como um pilar fundamental na jornada de recuperação, oferecendo um espaço seguro e acolhedor para que indivíduos em situação de vício encontrem apoio, compreensão e ferramentas para lidar com os desafios da recuperação. Mais do que simples reuniões, representam uma comunidade de pessoas que compartilham experiências, oferecem incentivo mútuo e constroem laços de amizade e solidariedade, promovendo a mudança e a construção de uma vida livre das drogas.</p>
         <p><strong>Entendendo o Poder dos Grupos de Apoio:</strong></p>
         <p>O vício é uma doença complexa e multifacetada que transcende os limites da dependência física. Envolve aspectos psicológicos, sociais e emocionais que, se não forem devidamente explorados e tratados, podem comprometer o processo de recuperação. É nesse contexto que os Grupos de Apoio assumem um papel crucial, oferecendo um ambiente propício para compartilhamento de experiências, apoio mútuo, motivação e incentivo, desenvolvimento de habilidades, aprendizado com a experiência de outros</p>
         <p><strong>Diversidade de Grupos:</strong></p>
         <p>Existem diversos tipos de Grupos de Apoio disponíveis, atendendo às diferentes necessidades e características dos indivíduos em situação de vício. Alguns grupos são específicos para determinados tipos de substâncias, enquanto outros são mais abrangentes e acolhem pessoas com diferentes tipos de vícios. É importante buscar o grupo que melhor se encaixa nas suas necessidades e expectativas.</p>
+          <button onClick={handleToggleText10}>Mostrar Menos</button>
+          </>
+        ) : (
+          <>
+        <p><strong>A Força da Comunidade na Jornada de Recuperação:</strong></p>
+        <p>Na luta contra o vício de substâncias ilegais, os Grupos de Apoio se erguem como um pilar fundamental na jornada de recuperação, oferecendo um espaço seguro e acolhedor para que indivíduos em situação de vício encontrem apoio, compreensão e ferramentas para lidar com os desafios da recuperação. Mais do que simples reuniões, representam uma comunidade de pessoas que compartilham experiências, oferecem incentivo mútuo e constroem laços de amizade e solidariedade, promovendo a mudança e a construção de uma vida livre das drogas.</p>
+        <p><strong>Entendendo o Poder dos Grupos de Apoio:</strong></p>
+        <p>O vício é uma doença complexa e multifacetada que transcende os limites da dependência física. Envolve...</p>
+        <button onClick={handleToggleText10}>Mostrar Mais</button>
+          </>
+        )
+      }
   
       </div>
       <div className="boxIeD">
         <h2>Suporte Familiar</h2>
+        {showFullText11 ? (
+          <>
         <p><strong>Pilar Fundamental na Jornada de Recuperação:</strong></p>
         <p>Na luta contra o vício de substâncias ilegais, o Suporte Familiar se ergue como um pilar fundamental na jornada de recuperação, oferecendo ao indivíduo em situação de vício uma rede de amor, apoio e compreensão que o auxilia a superar os desafios e construir uma vida livre das drogas. Mais do que simples presença física, o Suporte Familiar representa um conjunto de ações e atitudes que demonstram amor, cuidado e confiança no potencial do indivíduo para se recuperar.</p>
         <p><strong>Compreendendo a Importância do Suporte Familiar:</strong></p>
         <p>O vício é uma doença complexa que afeta não apenas o indivíduo que usa drogas, mas também toda a família. As consequências do vício podem se manifestar de diversas formas, como problemas financeiros, conflitos familiares, violência doméstica e negligência de crianças. Nesse contexto, o Suporte Familiar assume um papel crucial para promover um ambiente familiar acolhedor e seguro, oferecer apoio emocional, incentivar a busca por ajuda profissional, auxiliar na organização e acompanhamento do tratamento, celebrar conquistas e progressos, manter a esperança e a positividade</p>
         <p><strong>Como Oferecer Suporte Familiar:</strong></p>
         <p>Existem diversas maneiras de oferecer Suporte Familiar a um indivíduo em situação de vício. Algumas dicas importantes incluem: educar-se sobre o vício, comunicar-se de forma clara e honesta, estabelecer limites claros, evitar a culpabilização e a crítica, cuidar de si mesmo</p>
+          <button onClick={handleToggleText11}>Mostrar Menos</button>
+          </>
+        ) : (
+          <>
+        <p><strong>Pilar Fundamental na Jornada de Recuperação:</strong></p>
+        <p>Na luta contra o vício de substâncias ilegais, o Suporte Familiar se ergue como um pilar fundamental na jornada de recuperação, oferecendo ao indivíduo em situação de vício uma rede de amor, apoio e compreensão que o auxilia a superar os desafios e construir uma vida livre das drogas. Mais do que simples presença física, o Suporte Familiar representa um conjunto de ações e atitudes que demonstram amor, cuidado e confiança no potencial do indivíduo para se recuperar.</p>
+        <p><strong>Compreendendo a Importância do Suporte Familiar:</strong></p>
+        <p>O vício é uma doença complexa que afeta não apenas o indivíduo que usa drogas...</p>
+        <button onClick={handleToggleText11}>Mostrar Mais</button>
+          </>
+        )
+      }        
         </div>
+
       <div className="boxIeD">
         <h2>Recursos Online e Linhas de Ajuda</h2>
+        {showFullText11 ? (
+          <>
         <p><strong>Apoio na Palma da Sua Mão:</strong></p>
         <p>Na luta contra o vício de substâncias ilegais, os Recursos Online e Linhas de Ajuda se erguem como ferramentas valiosas para auxiliar indivíduos em situação de vício e seus familiares na jornada de recuperação. Através da internet e do telefone, é possível ter acesso a um leque de informações, serviços e apoio emocional, tudo na palma da sua mão, 24 horas por dia, 7 dias por semana.</p>
         <p><strong>Diversidade de Recursos:</strong></p>
@@ -80,6 +161,21 @@ function SupportPage() {
         <p><strong>Grupos de Apoio para Familiares e Amigos de Alcoólicos e Drogaditos (GRAAAD): (11) 3322-4444</strong></p>
         <p><strong>Benefícios dos Recursos Online e Linhas de Ajuda:</strong></p>
         <p>Os Recursos Online e Linhas de Ajuda oferecem diversos benefícios para quem está enfrentando o vício: acessibilidade, anonimato, imediação, flexibilidade, complementaridade</p>
+          <button onClick={handleToggleText11}>Mostrar Menos</button>
+          </>
+        ) : (
+          <>
+        <p><strong>Apoio na Palma da Sua Mão:</strong></p>
+        <p>Na luta contra o vício de substâncias ilegais, os Recursos Online e Linhas de Ajuda se erguem como ferramentas valiosas para auxiliar indivíduos em situação de vício e seus familiares na jornada de recuperação. Através da internet e do telefone, é possível ter acesso a um leque de informações, serviços e apoio emocional, tudo na palma da sua mão, 24 horas por dia, 7 dias por semana.</p>
+        <p><strong>Diversidade de Recursos:</strong></p>
+        <p>A internet oferece uma variedade de Recursos Online que podem ser úteis para quem está enfrentando o vício:</p>
+        <p><strong>Sites de informação:</strong> Diversos sites fornecem informações confiáveis sobre o vício, seus efeitos...</p>
+        <button onClick={handleToggleText11}>Mostrar Mais</button>
+          </>
+        )
+      }        
+        
+        
       </div>
     </div>
     )
